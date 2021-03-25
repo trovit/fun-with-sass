be4f34c632f85a25bd74bc6a0fa8ee99300379e2
Mario Sánchez <mr.mario.sanchez@gmail.com>
2021-03-25 18:40:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Fun with Sass
+
+This is a project to practice Sass basics in a mob with yout team to discover what Sass can offer to you and find out if it's valuable for your styles use cases.
+
+## Requierements
+* Last node.js LTS version.
+
+## Run the project
+
+1. Clone this repo.
+1. Enter in the project root.
+1. Execute `npm i` to install dependencies.
+1. Execute `npm start` to run the project in local, it will be automatically openend in your browser.
+1. Happy coding!
+
+## Resources
+* [Sass docs](https://sass-lang.com/documentation) 
+    * [Nesting](https://sass-lang.com/documentation/style-rules)
+    * [Parent selectors](https://sass-lang.com/documentation/style-rules/parent-selector)
+    * @extend: https://sass-lang.com/documentation/at-rules/extend
---
+++ src/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { render } from "react-dom";
+import MaterialUiButton from "@material-ui/core/Button";
+import "./base.css";
+
+import "./styles.scss";
+
+const App = () => (
+  <div className="container">
+    <Button>I am a Proppit Button :)</Button>
+    <Button color="secondary">I am a secondary Proppit Button :/</Button>
+    <Button disabled>I am a disabled Proppit Button :(</Button>
+    <Button variant="outlined">I am a outlined Proppit Button :O</Button>
+    <Button variant="text">I am a text Proppit Button :I</Button>
+  </div>
+);
+
+function Button({
+  children,
+  fullWidth,
+  onClick,
+  disabled,
+  type,
+  icon,
+  dataTest,
+  name,
+  variant = "contained",
+  color = "primary",
+  component,
+  to
+}) {
+  let innerButtonType = type;
+  if (type === "button") {
+    innerButtonType = undefined;
+  }
+
+  return (
+    <MaterialUiButton
+      classes={{
+        outlinedSecondary: "Button--outlined-secondary",
+        containedPrimary: "Button--contained-primary",
+        containedSecondary: "Button--contained-secondary",
+        root: "Button",
+        text: "Button--variant-text",
+        outlined: "Button--variant-outlined",
+        disabled: "Button--disabled"
+      }}
+      name={name}
+      variant={variant}
+      color={color}
+      fullWidth={fullWidth}
+      onClick={onClick}
+      disabled={disabled}
+      type={innerButtonType}
+      startIcon={icon}
+      data-test={dataTest}
+      component={component}
+      to={to}
+    >
+      {children}
+    </MaterialUiButton>
+  );
+}
+
+render(<App />, document.getElementById("app"));
